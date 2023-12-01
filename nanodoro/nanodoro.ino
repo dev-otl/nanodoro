bf987e036bf7e1b76d61f551a408121a4a8a486b
onthelearn <139883359+onthelearn@users.noreply.github.com>
2023-12-01 01:16:08 +0100

    copied codes from my esp32 project
    working but polling instead of interrupts, millis check instead of timer callback.
    functionally same, slow reaction issues. wait next second tick for any updates.
    not instant reaction to button press.

--- nanodoro/nanodoro.ino
+++ nanodoro/nanodoro.ino
@@ -58,6 +58,40 @@ static const unsigned char PROGMEM logo_bmp[] = {
   0b00000000, 0b00110000
 };
 
+#define PIN_SPEAKER 15
+#define PIN_PB_0_0 2
+#define PIN_PB_0_1 4
+#define PIN_PB_1_0 10
+#define PIN_PB_1_1 12
+
+#define TIME_FOCUS 25 * 60
+#define TIME_SHORTBREAK 5 * 60
+#define TIME_LONGBREAK 15 * 60
+
+typedef enum {
+  POMO_STATE_INIT,
+  POMO_STATE_RUNNING,
+  POMO_STATE_PAUSED,
+  POMO_STATE_STOPPED
+} pomo_state_t;
+
+pomo_state_t currentState = POMO_STATE_INIT;
+
+int play_pause_button_pressed = 0;
+int stop_button_pressed = 0;
+
+int currentTimer;
+int currentSession;
+int should_beep = 0;
+int is_beeping = 0;
+
+char timeString[5];
+
+unsigned long prevTime = 0;
+unsigned long prevTimeSpeaker = 0;
+unsigned long currentTime = 0;
+unsigned long currentTimeSpeaker = 0;
+
 void setup() {
   Serial.begin(9600);
 
@@ -68,356 +102,161 @@ void setup() {
       ;  // Don't proceed, loop forever
   }
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(2000);  // Pause for 2 seconds
+  //init speaker
+  pinMode(PIN_SPEAKER, OUTPUT);
+  digitalWrite(PIN_SPEAKER, 1);  //off
+
+
+  //init buttons
+  pinMode(PIN_PB_0_0, OUTPUT);
+  digitalWrite(PIN_PB_0_0, 0);  //button down -> low
+  pinMode(PIN_PB_0_1, INPUT_PULLUP);
+
+  pinMode(PIN_PB_1_0, OUTPUT);
+  digitalWrite(PIN_PB_1_0, 0);  //button down -> low
+  pinMode(PIN_PB_1_1, INPUT_PULLUP);
 
   // Clear the buffer
   display.clearDisplay();
 
-  /*
-  // Draw a single pixel in white
-  display.drawPixel(10, 10, SSD1306_WHITE);
+  currentTimer = TIME_FOCUS;
+  currentSession = 0;
+  play_pause_button_pressed = 0;
+  stop_button_pressed = 0;
 
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
-  display.display();
-  delay(2000);
-  // display.display() is NOT necessary after every single drawing command,
-  // unless that's what you want...rather, you can batch up a bunch of
-  // drawing operations and then update the screen all at once by calling
-  // display.display(). These examples demonstrate both approaches...
+  updateDisplay();
 
-  testdrawline();  // Draw many lines
+  prevTimeSpeaker = prevTime = millis();
 
-  testdrawrect();  // Draw rectangles (outlines)
-
-  testfillrect();  // Draw rectangles (filled)
-
-  testdrawcircle();  // Draw circles (outlines)
-
-  testfillcircle();  // Draw circles (filled)
-
-  testdrawroundrect();  // Draw rounded rectangles (outlines)
-
-  testfillroundrect();  // Draw rounded rectangles (filled)
-
-  testdrawtriangle();  // Draw triangles (outlines)
-
-  testfilltriangle();  // Draw triangles (filled)
-
-  testdrawchar();  // Draw characters of the default font
-
-  testdrawstyles();  // Draw 'stylized' characters
-
-  testscrolltext();  // Draw scrolling text
-
-  testdrawbitmap();  // Draw a small bitmap image
-
-  // Invert and restore display, pausing in-between
-  display.invertDisplay(true);
-  delay(1000);
-  display.invertDisplay(false);
-  delay(1000);
-
-  testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT);  // Animate bitmaps
-  */
-
-
+  for (;;) {  //timer logic
+    if (!digitalRead(PIN_PB_0_1)) play_pause_button_pressed = 1;
+    if (!digitalRead(PIN_PB_1_1)) stop_button_pressed = 1;
+    currentTimeSpeaker = currentTime = millis();
+    if (currentTime - prevTime >= 1000) {  //1s timer time up
+      prevTime = currentTime;
+      pomodoro_system_timer_callback();
+    }
+    if (currentTimeSpeaker - prevTimeSpeaker >= 250) {
+      prevTimeSpeaker = currentTimeSpeaker;
+      if (should_beep) pomodoro_speaker_timer_callback();
+      else digitalWrite(PIN_SPEAKER, 1);  //off
+    }
+  }
 }
 
 void loop() {
-  
 }
 
-void testdrawline() {
-  int16_t i;
-
-  display.clearDisplay();  // Clear display buffer
-
-  for (i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, 0, i, display.height() - 1, SSD1306_WHITE);
-    display.display();  // Update screen with each newly-drawn line
-    delay(1);
-  }
-  for (i = 0; i < display.height(); i += 4) {
-    display.drawLine(0, 0, display.width() - 1, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for (i = 0; i < display.width(); i += 4) {
-    display.drawLine(0, display.height() - 1, i, 0, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(0, display.height() - 1, display.width() - 1, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for (i = display.width() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, i, 0, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (i = display.height() - 1; i >= 0; i -= 4) {
-    display.drawLine(display.width() - 1, display.height() - 1, 0, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  delay(250);
-
-  display.clearDisplay();
-
-  for (i = 0; i < display.height(); i += 4) {
-    display.drawLine(display.width() - 1, 0, 0, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-  for (i = 0; i < display.width(); i += 4) {
-    display.drawLine(display.width() - 1, 0, i, display.height() - 1, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);  // Pause for 2 seconds
+void ISR_pb0() {
+  play_pause_button_pressed = 1;
 }
 
-void testdrawrect(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < display.height() / 2; i += 2) {
-    display.drawRect(i, i, display.width() - 2 * i, display.height() - 2 * i, SSD1306_WHITE);
-    display.display();  // Update screen with each newly-drawn rectangle
-    delay(1);
-  }
-
-  delay(2000);
+void ISR_pb1() {
+  stop_button_pressed = 1;
 }
 
-void testfillrect(void) {
+
+void updateDisplay(void) {
   display.clearDisplay();
-
-  for (int16_t i = 0; i < display.height() / 2; i += 3) {
-    // The INVERSE color is used so rectangles alternate white/black
-    display.fillRect(i, i, display.width() - i * 2, display.height() - i * 2, SSD1306_INVERSE);
-    display.display();  // Update screen with each newly-drawn rectangle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawcircle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < max(display.width(), display.height()) / 2; i += 2) {
-    display.drawCircle(display.width() / 2, display.height() / 2, i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillcircle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = max(display.width(), display.height()) / 2; i > 0; i -= 3) {
-    // The INVERSE color is used so circles alternate white/black
-    display.fillCircle(display.width() / 2, display.height() / 2, i, SSD1306_INVERSE);
-    display.display();  // Update screen with each newly-drawn circle
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawroundrect(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    display.drawRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i,
-                          display.height() / 4, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfillroundrect(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < display.height() / 2 - 2; i += 2) {
-    // The INVERSE color is used so round-rects alternate white/black
-    display.fillRoundRect(i, i, display.width() - 2 * i, display.height() - 2 * i,
-                          display.height() / 4, SSD1306_INVERSE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawtriangle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = 0; i < max(display.width(), display.height()) / 2; i += 5) {
-    display.drawTriangle(
-      display.width() / 2, display.height() / 2 - i,
-      display.width() / 2 - i, display.height() / 2 + i,
-      display.width() / 2 + i, display.height() / 2 + i, SSD1306_WHITE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testfilltriangle(void) {
-  display.clearDisplay();
-
-  for (int16_t i = max(display.width(), display.height()) / 2; i > 0; i -= 5) {
-    // The INVERSE color is used so triangles alternate white/black
-    display.fillTriangle(
-      display.width() / 2, display.height() / 2 - i,
-      display.width() / 2 - i, display.height() / 2 + i,
-      display.width() / 2 + i, display.height() / 2 + i, SSD1306_INVERSE);
-    display.display();
-    delay(1);
-  }
-
-  delay(2000);
-}
-
-void testdrawchar(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);               // Normal 1:1 pixel scale
+  display.setTextSize(4);               // Normal 1:1 pixel scale
   display.setTextColor(SSD1306_WHITE);  // Draw white text
-  display.setCursor(0, 0);              // Start at top-left corner
+  display.setCursor(6, 15);             // Start at top-left corner
   display.cp437(true);                  // Use full 256 char 'Code Page 437' font
 
   // Not all the characters will fit on the display. This is normal.
   // Library will draw what it can and the rest will be clipped.
-  for (int16_t i = 0; i < 256; i++) {
-    if (i == '\n') display.write(' ');
-    else display.write(i);
+  sprintf(timeString,
+          "%d%d:%d%d",
+          (currentTimer / 60) / 10,
+          (currentTimer / 60) % 10,
+          (currentTimer % 60) / 10,
+          (currentTimer % 60) % 10);
+  display.write(timeString);
+
+  display.display();
+}
+
+int getCurrentSessionTime() {
+  switch (currentSession) {
+    case 0:
+    case 2:
+    case 4:
+    case 6:
+      return TIME_FOCUS;
+      break;
+    case 1:
+    case 3:
+    case 5:
+      return TIME_SHORTBREAK;
+      break;
+    case 7:
+      return TIME_LONGBREAK;
+      break;
+    default:
+      return 0;
+      break;
   }
-
-  display.display();
-  delay(2000);
+  return 0;
 }
 
-void testdrawstyles(void) {
-  display.clearDisplay();
-
-  display.setTextSize(1);               // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);  // Draw white text
-  display.setCursor(0, 0);              // Start at top-left corner
-  display.println(F("Hello, world!"));
-
-  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);  // Draw 'inverse' text
-  display.println(3.141592);
-
-  display.setTextSize(2);  // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.print(F("0x"));
-  display.println(0xDEADBEEF, HEX);
-
-  display.display();
-  delay(2000);
-}
-
-void testscrolltext(void) {
-  display.clearDisplay();
-
-  display.setTextSize(2);  // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(10, 0);
-  display.println(F("scroll"));
-  display.display();  // Show initial text
-  delay(100);
-
-  // Scroll in various directions, pausing in-between:
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-}
-
-void testdrawbitmap(void) {
-  display.clearDisplay();
-
-  display.drawBitmap(
-    (display.width() - LOGO_WIDTH) / 2,
-    (display.height() - LOGO_HEIGHT) / 2,
-    logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
-  display.display();
-  delay(1000);
-}
-
-#define XPOS 0  // Indexes into the 'icons' array in function below
-#define YPOS 1
-#define DELTAY 2
-
-void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-  int8_t f, icons[NUMFLAKES][3];
-
-  // Initialize 'snowflake' positions
-  for (f = 0; f < NUMFLAKES; f++) {
-    icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
-    icons[f][YPOS] = -LOGO_HEIGHT;
-    icons[f][DELTAY] = random(1, 6);
-    Serial.print(F("x: "));
-    Serial.print(icons[f][XPOS], DEC);
-    Serial.print(F(" y: "));
-    Serial.print(icons[f][YPOS], DEC);
-    Serial.print(F(" dy: "));
-    Serial.println(icons[f][DELTAY], DEC);
-  }
-
-  for (;;) {                 // Loop forever...
-    display.clearDisplay();  // Clear the display buffer
-
-    // Draw each snowflake:
-    for (f = 0; f < NUMFLAKES; f++) {
-      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
-    }
-
-    display.display();  // Show the display buffer on the screen
-    delay(200);         // Pause for 1/10 second
-
-    // Then update coordinates of each flake...
-    for (f = 0; f < NUMFLAKES; f++) {
-      icons[f][YPOS] += icons[f][DELTAY];
-      // If snowflake is off the bottom of the screen...
-      if (icons[f][YPOS] >= display.height()) {
-        // Reinitialize to a random position, just off the top
-        icons[f][XPOS] = random(1 - LOGO_WIDTH, display.width());
-        icons[f][YPOS] = -LOGO_HEIGHT;
-        icons[f][DELTAY] = random(1, 6);
+void pomodoro_system_timer_callback() {
+  should_beep = 1;
+  switch (currentState) {
+    case POMO_STATE_INIT:
+      should_beep = 0;
+      currentSession = 0;
+      currentTimer = getCurrentSessionTime();
+      if (play_pause_button_pressed) {
+        currentState = POMO_STATE_RUNNING;
+        play_pause_button_pressed = 0;
       }
-    }
+      if (stop_button_pressed) {
+        stop_button_pressed = 0;
+      }
+      break;
+    case POMO_STATE_RUNNING:
+      should_beep = 0;
+      if (--currentTimer == 0) {
+        currentState = POMO_STATE_STOPPED;
+        currentSession = (currentSession + 1) % 8;
+        currentTimer = getCurrentSessionTime();
+      } else {
+        if (play_pause_button_pressed) {
+          currentState = POMO_STATE_PAUSED;
+          play_pause_button_pressed = 0;
+        }
+        if (stop_button_pressed) {
+          currentState = POMO_STATE_STOPPED;
+          currentTimer = getCurrentSessionTime();
+          stop_button_pressed = 0;
+        }
+      }
+      break;
+    case POMO_STATE_PAUSED:
+      if (play_pause_button_pressed) {
+        currentState = POMO_STATE_RUNNING;
+        play_pause_button_pressed = 0;
+      }
+      if (stop_button_pressed) {
+        currentState = POMO_STATE_STOPPED;
+        currentTimer = getCurrentSessionTime();
+        stop_button_pressed = 0;
+      }
+      break;
+    case POMO_STATE_STOPPED:
+      if (play_pause_button_pressed) {
+        currentState = POMO_STATE_RUNNING;
+        play_pause_button_pressed = 0;
+      }
+      break;
+    default:
+      break;
   }
+  play_pause_button_pressed = stop_button_pressed = 0;
+  updateDisplay();
+}
+
+void pomodoro_speaker_timer_callback() {
+  is_beeping ^= 1;
+  digitalWrite(PIN_SPEAKER, is_beeping);
 }
